8d571983cd632531de2f395123ca5e5f2328b94f
machiav3lli <antonios.hazim@tutanota.com>
2024-11-23 21:13:04 +0100

    Update: Share button to MD3

--- src/renderer/components/ft-share-button/ft-share-button.vue
+++ src/renderer/components/ft-share-button/ft-share-button.vue
@@ -9,112 +9,114 @@
     :dropdown-position-y="dropdownPositionY"
     :force-dropdown="true"
   >
-    <ft-flex-box>
-      <ft-toggle-switch
-        v-if="isVideo"
-        :label="$t('Share.Include Timestamp')"
-        :compact="true"
-        :default-value="includeTimestamp"
-        @change="updateIncludeTimestamp"
-      />
-    </ft-flex-box>
-    <div class="shareLinks">
-      <div class="header">
-        <img
-          id="youtubeShareImage"
-          class="youtubeLogo"
-          src="~../../assets/img/yt_logo_mono_dark.png"
-          alt="YouTube"
-          width="794"
-          height="178"
-        >
+    <div>
+      <div class="center">
+        <ft-toggle-switch
+          v-if="isVideo"
+          :label="$t('Share.Include Timestamp')"
+          :compact="true"
+          :default-value="includeTimestamp"
+          @change="updateIncludeTimestamp"
+        />
       </div>
+      <div class="shareLinks">
+        <div
+          id="youtubeShare"
+          class="header shareTitle"
+        >
+          <span class="shareLogo youtubeLogo" /> YouTube
+        </div>
 
-      <div class="buttons">
-        <ft-button
-          class="action"
-          aria-describedby="youtubeShareImage"
-          @click="copyYoutube()"
-        >
-          <font-awesome-icon :icon="['fas', 'copy']" />
-          {{ $t("Share.Copy Link") }}
-        </ft-button>
-        <ft-button
-          class="action"
-          aria-describedby="youtubeShareImage"
-          @click="openYoutube()"
-        >
-          <font-awesome-icon :icon="['fas', 'globe']" />
-          {{ $t("Share.Open Link") }}
-        </ft-button>
-        <ft-button
-          v-if="isVideo || isPlaylist"
-          class="action"
-          aria-describedby="youtubeShareImage"
-          background-color="var(--accent-color-active)"
-          @click="copyYoutubeEmbed()"
-        >
-          <font-awesome-icon :icon="['fas', 'copy']" />
-          {{ $t("Share.Copy Embed") }}
-        </ft-button>
-        <ft-button
-          v-if="isVideo || isPlaylist"
-          class="action"
-          aria-describedby="youtubeShareImage"
-          background-color="var(--accent-color-active)"
-          @click="openYoutubeEmbed()"
-        >
-          <font-awesome-icon :icon="['fas', 'globe']" />
-          {{ $t("Share.Open Embed") }}
-        </ft-button>
-      </div>
+        <div class="buttons">
+          <ft-button
+            class="action"
+            aria-describedby="youtubeShareImage"
+            @click="copyYoutube()"
+          >
+            <font-awesome-icon :icon="['fas', 'copy']" />
+            {{ $t("Share.Copy Link") }}
+          </ft-button>
+          <ft-button
+            class="action"
+            aria-describedby="youtubeShareImage"
+            @click="openYoutube()"
+          >
+            <font-awesome-icon :icon="['fas', 'globe']" />
+            {{ $t("Share.Open Link") }}
+          </ft-button>
+          <ft-button
+            v-if="isVideo || isPlaylist"
+            class="action"
+            aria-describedby="youtubeShareImage"
+            background-color="var(--color-secondary)"
+            text-color="var(--color-on-secondary)"
+            @click="copyYoutubeEmbed()"
+          >
+            <font-awesome-icon :icon="['fas', 'copy']" />
+            {{ $t("Share.Copy Embed") }}
+          </ft-button>
+          <ft-button
+            v-if="isVideo || isPlaylist"
+            class="action"
+            aria-describedby="youtubeShareImage"
+            background-color="var(--color-secondary)"
+            text-color="var(--color-on-secondary)"
+            @click="openYoutubeEmbed()"
+          >
+            <font-awesome-icon :icon="['fas', 'globe']" />
+            {{ $t("Share.Open Embed") }}
+          </ft-button>
+        </div>
 
-      <div class="divider" />
+        <div class="divider" />
 
-      <div
-        id="invidiousShare"
-        class="header invidious"
-      >
-        <span class="invidiousLogo" /> Invidious
-      </div>
+        <div
+          id="invidiousShare"
+          class="header shareTitle"
+        >
+          <span class="shareLogo invidiousLogo" /> Invidious
+        </div>
 
-      <div class="buttons">
-        <ft-button
-          aria-describedby="invidiousShare"
-          class="action"
-          @click="copyInvidious()"
-        >
-          <font-awesome-icon :icon="['fas', 'copy']" />
-          {{ $t("Share.Copy Link") }}
-        </ft-button>
-        <ft-button
-          aria-describedby="invidiousShare"
-          class="action"
-          @click="openInvidious()"
-        >
-          <font-awesome-icon :icon="['fas', 'globe']" />
-          {{ $t("Share.Open Link") }}
-        </ft-button>
-        <ft-button
-          v-if="isVideo || isPlaylist"
-          aria-describedby="invidiousShare"
-          class="action"
-          background-color="var(--accent-color-active)"
-          @click="copyInvidiousEmbed()"
-        >
-          <font-awesome-icon :icon="['fas', 'copy']" />
-          {{ $t("Share.Copy Embed") }}
-        </ft-button>
-        <ft-button
-          v-if="isVideo || isPlaylist"
-          aria-describedby="invidiousShare"
-          class="action"
-          background-color="var(--accent-color-active)"
-          @click="openInvidiousEmbed()"
-        >
-          <font-awesome-icon :icon="['fas', 'globe']" />
-          {{ $t("Share.Open Embed") }}
-        </ft-button>
+        <div class="buttons">
+          <ft-button
+            aria-describedby="invidiousShare"
+            class="action"
+            @click="copyInvidious()"
+          >
+            <font-awesome-icon :icon="['fas', 'copy']" />
+            {{ $t("Share.Copy Link") }}
+          </ft-button>
+          <ft-button
+            aria-describedby="invidiousShare"
+            class="action"
+            @click="openInvidious()"
+          >
+            <font-awesome-icon :icon="['fas', 'globe']" />
+            {{ $t("Share.Open Link") }}
+          </ft-button>
+          <ft-button
+            v-if="isVideo || isPlaylist"
+            aria-describedby="invidiousShare"
+            class="action"
+            background-color="var(--color-secondary)"
+            text-color="var(--color-on-secondary)"
+            @click="copyInvidiousEmbed()"
+          >
+            <font-awesome-icon :icon="['fas', 'copy']" />
+            {{ $t("Share.Copy Embed") }}
+          </ft-button>
+          <ft-button
+            v-if="isVideo || isPlaylist"
+            aria-describedby="invidiousShare"
+            class="action"
+            background-color="var(--color-secondary)"
+            text-color="var(--color-on-secondary)"
+            @click="openInvidiousEmbed()"
+          >
+            <font-awesome-icon :icon="['fas', 'globe']" />
+            {{ $t("Share.Open Embed") }}
+          </ft-button>
+        </div>
       </div>
     </div>
   </ft-icon-button>
